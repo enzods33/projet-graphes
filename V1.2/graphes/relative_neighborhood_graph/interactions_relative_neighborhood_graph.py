b3bcaedc1b038e7f2aa47fd6db76ae7d705e5d1e
dams-ez <laniodamien@gmail.com>
2025-04-30 15:44:54 +0200

    relative neighborghoof

--- V1.2/graphes/relative_neighborhood_graph/interactions_relative_neighborhood_graph.py
+++ V1.2/graphes/relative_neighborhood_graph/interactions_relative_neighborhood_graph.py
@@ -1,1 +1,22 @@
 # Interactions pour le graphe relative_neighborhood_graph
+import tkinter as tk
+from interface_graphique import interactions_canvas as ic
+
+def is_connected(p1, p2):
+    """
+    Détermine si p1 et p2 doivent être connectés
+    """
+    distance_p1_p2 = ic.get_distance_reelle(p1, p2)
+    for r in ic.sommets:
+        if r != p1 and r != p2:
+            if max(ic.get_distance_reelle(p1, r), ic.get_distance_reelle(p2, r)) <distance_p1_p2:
+                return False
+    return True
+    
+
+
+def get_type_graphe():
+    """
+    Retourne le type du graphe actuellement utilisé ("Relative neighborhood graph").
+    """
+    return "Relative neighborhood graph"
